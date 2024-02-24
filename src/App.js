f0b4b2b99ea2f1131b77aaa208186e4a29d9430d
IshworKhadka <ishworfloyd@gmail.com>
2024-02-24 21:18:14 +1030

    playing around with first component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleNameChange = () =>  {
+    const names = ['Ram', 'Sita', 'Laxman', 'Ravan'];
+    const num = Math.floor(Math.random() * 4);
+    return names[num];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>Namaste {handleNameChange()}</h3>
+        <h4>हैट, कडा रहेछ!</h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see changes.
         </p>
         <a
           className="App-link"
@@ -15,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          सिक्ने हैन त रियाक्ट?
         </a>
       </header>
     </div>
